c97ed1e94444a60c98002fc384965f47979c96a3
Yash Choudhary <125878202+yashu7470@users.noreply.github.com>
2023-02-24 22:38:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -82,86 +82,8 @@ function ShowPic() {
     }
 }
 
-// function ToggleReadMode() {
 
 
-
-//     if (document.body.style.filter != 'none') {
-
-//         document.body.style.filter = "none";
-
-
-//     } else {
-
-//         document.body.style.filter = "brightness(50%)";
-
-//     }
-
-
-// }
-
-
-function ToggleReadMode() {
-
-
-
-    if (document.body.style.filter != 'none') {
-
-        document.body.style.filter = "none";
-
-
-    } else {
-
-        document.body.style.filter = "brightness(50%)";
-
-    }
-
-
-}
-
-
-function changePP() {
-    if (document.querySelector('.resumesec').style.boxShadow != 'none') {
-
-        document.querySelector('.resumesec').style.boxShadow = "none";
-        document.querySelector('.contactus').style.boxShadow = "none";
-
-
-    }
-
-
-    else {
-        document.querySelector('.resumesec').style.boxShadow = '2px 2px 30px blue ';
-        document.querySelector('.contactus').style.boxShadow = "3px 3px 20px blue";
-
-    }
-
-}
-function hide() {
-    if (document.getElementById('downsec').style.display != 'none') {
-        document.getElementById('downsec').style.display = 'none';
-    }
-    else {
-        document.getElementById('downsec').style.display = 'flex';
-    }
-}
-
-function crc() {
-    if (document.querySelector('.resumesec').style.backgroundColor != 'black') {
-
-        document.querySelector('.resumesec').style.backgroundColor = 'black';
-        document.querySelector('.resumesec').style.color = 'cadetblue';
-        
-
-    }
-    else {
-        document.querySelector('.resumesec').style.backgroundColor = 'white';
-        document.querySelector('.resumesec').style.color = 'black';
-        
-
-
-    }
-}
 
 var btnsound = document.getElementById("keypress");
 
